f7fd29a840585ccb642fb73141fefcff1ef83194
이동욱 <du123kim@naver.com>
2024-06-04 13:07:08 +0900

    해결

--- Homepage.py
+++ Homepage.py
@@ -2,8 +2,8 @@ from flask import session, render_template
 
 def homepage():
     if 'username' not in session:
-        print("No username in session")  # 디버깅 메시지
+        print("No username in session") 
         return render_template('not_login_homepage.html')
     else:
-        print("Username in session:", session['username'])  # 디버깅 메시지
+        print("Username in session:", session['username'])  
         return render_template('login_homepage.html')
