d5425e5cd8451194c010612ea747c6da54b25725
이동욱 <du123kim@naver.com>
2024-06-06 10:19:17 +0900

    이미지 화면에 업로드

--- Homepage.py
+++ Homepage.py
@@ -1,9 +1,16 @@
 from flask import session, render_template
+import pymongo
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://leedonguk:1234@cluster0.xreq6mx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = cluster["term_project"]
+photo_collection = db["Photo"]
 
 def homepage():
     if 'username' not in session:
-        print("No username in session") 
+        print("No username in session")
         return render_template('not_login_homepage.html')
     else:
-        print("Username in session:", session['username'])  
-        return render_template('login_homepage.html')
+        print("Username in session:", session['username'])
+        photos = list(photo_collection.find({}))
+        return render_template('login_homepage.html', photos=photos)
